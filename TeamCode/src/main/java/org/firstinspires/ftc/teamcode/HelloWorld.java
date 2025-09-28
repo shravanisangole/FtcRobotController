f714ab5ede9196d43f653301101c3b18e91934c6
shravanisangole <shravanissangole@gmail.com>
2025-09-28 13:54:45 -0500

    Hello World!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class HelloWorld extends OpMode {
-
+    //Test
     @Override
     public void init() {
         telemetry.addData("Hello", "Spart10 Code Team!");
